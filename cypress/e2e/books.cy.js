c70ebd09dc6eb3085ce39a49b2cf2a81a94cccd7
Galina Demidova <galinagriz77@gmail.com>
2022-12-06 16:10:29 +0300

    commit cypress 1

--- cypress/e2e/books.cy.js
+++ cypress/e2e/books.cy.js
@@ -1,3 +1,26 @@
+
+
+it('Add a book 1', () => {
+    cy.visit('/');
+    cy.login('test@test.com', 'test');
+    cy.books('Детство', 'Лев Толстой');
+    cy.contains('Детство').should('be.visible'); 
+});
+it('Add a book 2', () => {
+    cy.visit('/');
+    cy.login('test@test.com', 'test');
+    cy.books('Мишкина каша', 'Николай Носов');
+    cy.contains('Мишкина каша').should('be.visible'); 
+});
+
+it( 'Add book to favorites', () => {
+    cy.visit('/');
+    cy.login('test@test.com', 'test');
+    cy.contains('Add to favorite').click();
+    cy.contains('Favorites').click();
+    cy.contains('Delete from favorite').should('be.visible');
+});
+
 it('Should open the books page', () => {
     cy.visit('/');
     cy.contains('Books list').should('be.visible');
@@ -8,6 +31,7 @@ it('Success login', () => {
     cy.contains('test@test.com').should('be.visible');
     cy.contains('Add new').should('have.class', 'btn');
 });
+
 it('Wrong password', () => {
     cy.visit('/');
     cy.login('test@test.com');
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,11 +26,18 @@
 
 Cypress.Commands.add("login", (email, password) => {
     cy.contains("Log in").click();
+    
     if (email) {
     cy.get("#mail").type(email);
     }
     if (password) {
     cy.get("#pass").type(password);
     }
+    cy.contains("Submit").click();
+  });
+  Cypress.Commands.add("books", (title, author) => {
+    cy.contains("Add new").click();
+    cy.get("#title").type(title);
+    cy.get("#authors").type(author);
     cy.contains("Submit").click();
   });
